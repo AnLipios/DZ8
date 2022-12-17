f748d86c5c3d72ab5dd5dd8c14795259d24f3b9a
Anna <ogonek0918@yandex.ru>
2022-12-17 22:42:40 +0700

    task60

---
+++ task60/Program.cs
@@ -0,0 +1,80 @@
+﻿//Сформируйте трёхмерный массив из неповторяющихся 
+//двузначных чисел. Напишите программу, 
+//которая будет построчно выводить массив, 
+//добавляя индексы каждого элемента.
+
+using System;
+using static System.Console;
+
+Clear();
+int[,,] mass = new int [2,2,2];
+CreateArray (mass);
+FillArray(mass);
+PrintArray (mass);
+
+
+void PrintArray (int[,,] mass)
+{
+    for (int i = 0; i < mass.GetLength (0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            for (int k = 0; k< mass.GetLength(2); k++)
+            {
+                Write($"{mass[i,j,k]}=({i},{j},{k}); ");
+            }
+            WriteLine();
+        }
+        WriteLine();
+    }
+}
+
+void FillArray(int[,,]mass)
+{
+    for (int i=0; i<mass.GetLength(0); i++)
+    {
+        for (int j=0; j<mass.GetLength(1); j++)
+        {
+            for (int k=0; k < mass.GetLength(2); k++)
+            {
+                mass[i,j,k]=new Random().Next(1,100);
+            }
+        }
+    }
+}
+void CreateArray(int[,,]mass)
+{
+    int[] temp = new int[mass.GetLength(0) * mass.GetLength(1) * mass.GetLength(2)];
+    int number;
+    for (int i=0; i<temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(1, 100);
+        number = temp[i];
+        if (i>=1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i]= new Random().Next(1, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+
+    }
+    int count = 0;
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            for (int k =0; k < mass.GetLength(2); k++)
+            {
+                mass[i,j,k] = temp[count];
+                count++;
+            }
+        }
+    }
+}
