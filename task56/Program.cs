c06238d4e0919f920b37e2daa67429166e9dcbdc
Anna <ogonek0918@yandex.ru>
2022-12-17 22:41:33 +0700

    task56

---
+++ task56/Program.cs
@@ -0,0 +1,63 @@
+﻿
+//Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить 
+//строку с наименьшей суммой элементов.
+
+using System;
+using static System.Console;
+
+Clear();
+
+
+void PrintArray(int[,] mass)
+{
+    for (int i=0; i<mass.GetLength(0); i++)
+    {
+        for (int j=0; j< mass.GetLength(1); j++)
+        {
+            Write($"{mass[i,j]} ");
+            }
+            WriteLine();
+    }
+    
+}
+
+void FillArray(int[,]mass)
+{
+    for (int i=0; i<mass.GetLength(0); i++)
+    {
+        for (int j=0; j<mass.GetLength(1); j++)
+        {
+            mass[i,j]=new Random().Next(1,10);
+        }
+    }
+  
+}
+
+int GetSum(int[,]mass, int i)
+{
+    int sum = mass[i,0];
+    for (int j = 1; j < mass.GetLength(1); j++)
+    {
+        sum += mass[i,j];
+    }
+    return sum;
+}
+
+int[,] mass=new int[6,5];
+FillArray(mass);
+PrintArray (mass);
+WriteLine("----------");
+
+int minSum = 0;
+int sum = GetSum(mass, 0);
+for (int i = 1; i < mass.GetLength(0); i++)
+{
+    int tempSum = GetSum(mass, i);
+    if (sum > tempSum)
+    {
+        sum = tempSum;
+        minSum = i;
+    }
+}
+WriteLine($"Строка с наименьшей суммой элементов: {minSum+1}");
